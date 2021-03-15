5346a86fa60cc57633bde829627fc93bf9e7fca2
Bilal BAig <bilalbaig587@gmail.com>
2021-03-14 21:55:22 -0500

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Bilal Baig on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
